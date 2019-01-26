2545299e4779b9d495d5b145251271146f97eb39
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-01-25 18:37:53 -0600

    complete

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const table = document.getElementsByTagName('table')[0];
+const addRow = document.getElementById('add-row');
+const select = document.getElementsByTagName('select')[0];
 
 function makeRow() {
   const tr = document.createElement('tr');
@@ -9,21 +11,29 @@ function makeRow() {
   table.appendChild(tr);
 }
 
-const addRow = document.getElementById('add-row');
 addRow.addEventListener('click', makeRow);
 
 table.addEventListener('click', colorize);
 
 let chosenColor = 'red';
-const select = document.getElementsByTagName('select')[0];
 select.addEventListener('change', function(event) {
   chosenColor = event.target.value;
 });
 
 function colorize(event) {
-  if (event.target.className.length) {
-    event.target.className = '';
-  } else {
-    event.target.className = chosenColor;
+  if (event.target.tagName === 'TD') {
+    if (event.target.className === chosenColor) {
+      event.target.className = '';
+    } else {
+      event.target.className = chosenColor;
+    }
   }
 }
+
+table.addEventListener('mousedown', () => {
+  table.addEventListener('mouseover', colorize);
+});
+
+table.addEventListener('mouseup', () => {
+  table.removeEventListener('mouseover', colorize);
+});
